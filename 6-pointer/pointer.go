64ca35a2d9a5be2e24214eb3bf905a7abce88c55
DESKTOP-NTVI36V\hu <hubotao1998@163.com>
2022-07-08 19:24:25 +0800

    20220708-19
    指针和二级指针

--- 6-pointer/pointer.go
+++ 6-pointer/pointer.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func swap(a, b *int) {
 	temp := *a
 	*a = *b
@@ -11,4 +13,17 @@ func main() {
 	// swap
 	swap(&a, &b)
 	println("a =", a, "b =", b)
+
+	var p *int = &a
+
+	fmt.Println(&a)
+	fmt.Println(p)
+	fmt.Println("*p -> a =", *p)
+
+	//二级指针
+	var pp **int = &p
+
+	fmt.Println(&p)
+	fmt.Println(pp)
+	fmt.Println("**pp -> *p -> a =", **pp)
 }
