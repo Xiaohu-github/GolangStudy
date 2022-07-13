d35c518dd6701c6f539c9e455b820a31e115f539
DESKTOP-NTVI36V\hu <57241542+Xiaohu-github@users.noreply.github.com>
2022-07-13 22:11:40 +0800

    20220713-22

---
+++ 11-pair/2_pair.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Reader interface {
+	ReadBook()
+}
+
+type Writer interface {
+	WriterBook()
+}
+
+type Book struct {
+}
+
+func (this *Book) ReadBook() {
+	fmt.Println("Read a Book！")
+}
+
+func (this *Book) WriterBook() {
+	fmt.Println("Writer a book")
+}
+
+func main() {
+	//b:pair<type:Book,value:book{}地址>
+	// var b *Book = &Book{}
+	b := &Book{}
+	fmt.Printf("b type is %T\n", b)
+	b.ReadBook()
+
+	//定义了一个 Reader interface r
+	var r Reader //r：pair<type:,value:>
+
+	r = b //r:pair<type:Book,value:book{}地址>
+	fmt.Printf("r type is %T\n", r)
+	r.ReadBook()
+
+	//定义了一个 Reader interface w
+	var w Writer //w：pair<type:,value:>
+
+	w = r.(Writer) //w:pair<type:Book,value:book{}地址>
+
+	w.WriterBook()
+}
