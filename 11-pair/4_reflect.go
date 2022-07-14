2b07892ed9fdfbca2a592c736eb08c6ad447437e
DESKTOP-NTVI36V\hu <57241542+Xiaohu-github@users.noreply.github.com>
2022-07-15 07:56:22 +0800

    20220715-08

--- 11-pair/4_reflect.go
+++ 11-pair/4_reflect.go
@@ -11,13 +11,14 @@ type User struct {
 	Age  int
 }
 
-func (this *User) Call() {
+func (this User) Call() {
 	fmt.Println("User func called....")
 	fmt.Printf("%v\n", this)
 }
 
 func main() {
 	user := User{1, "HU", 24}
+	user.Call()
 	DofileAndMethod(user)
 }
 
@@ -32,12 +33,18 @@ func DofileAndMethod(input interface{}) {
 
 	//通过类型获取类型里面的字段
 	//1、获取变量的Type,通过Type得到NumField（字段总数）,然后遍历
-
 	for i := 0; i < inputType.NumField(); i++ {
 		//2、通过type得到每个filed（字段类型）
 		field := inputType.Field(i)
 		//3、通过Value得到字段的Interface()=>对应的value值
 		value := inputValue.Field(i).Interface()
-		fmt.Printf("%s ：%v\n", field.Name, field.Type, value)
+		fmt.Printf("%s ：%v = %v\n", field.Name, field.Type, value)
+	}
+
+	var num int = inputType.NumMethod()
+	//通过type 获取里面的方法
+	for i := 0; i < num; i++ {
+		m := inputType.Method(i)
+		fmt.Printf("%s:%v\n", m.Name, m.Type)
 	}
 }
