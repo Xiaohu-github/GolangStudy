259ce266fbb04fbb81ef1b09b11964f9aaf7464d
DESKTOP-NTVI36V\hu <57241542+Xiaohu-github@users.noreply.github.com>
2022-07-25 07:58:37 +0800

    20220725-08
    update user server go

---
+++ 16-project/user.go
@@ -0,0 +1,31 @@
+package main
+
+import "net"
+
+type User struct {
+	Name string
+	Addr string
+	C    chan string
+	conn net.Conn
+}
+
+func NewUser(conn net.Conn) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Name: userAddr,
+		Addr: userAddr,
+		C:    make(chan string),
+		conn: conn,
+	}
+
+	go user.ListenMessage()
+
+	return user
+}
+
+func (this *User) ListenMessage() {
+	for {
+		msg := <-this.C
+		this.conn.Write([]byte(msg + "\n"))
+	}
+}
