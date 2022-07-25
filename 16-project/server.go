f74c42a9fc87f6f67935af7d1e479f13c564c539
DESKTOP-NTVI36V\hu <57241542+Xiaohu-github@users.noreply.github.com>
2022-07-25 21:34:23 +0800

    20220725-21

--- 16-project/server.go
+++ 16-project/server.go
@@ -3,15 +3,57 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
-	Ip   string
-	Port int
+	Ip        string
+	Port      int
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+	Message   chan string
+}
+
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+
+	return server
+}
+
+func (this *Server) ListenMessage() {
+	for {
+		msg := <-this.Message
+
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	fmt.Println("来自客户端消息：", sendMsg)
+	this.Message <- sendMsg
 }
 
 func (this *Server) Handler(conn net.Conn) {
-	fmt.Println("连接建立成功")
+	// fmt.Println("连接建立成功")
+	user := NewUser(conn)
+	// fmt.Println("+++++>", user)
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	this.BroadCast(user, "已上线")
+
+	select {}
 }
 
 func (this *Server) Start() {
@@ -23,6 +65,8 @@ func (this *Server) Start() {
 
 	//defer 关闭 socket 监听
 	defer listener.Close()
+
+	go this.ListenMessage()
 
 	//循环运行
 	for {
@@ -36,9 +80,4 @@ func (this *Server) Start() {
 		//处理一些操作
 		go this.Handler(conn)
 	}
-}
-
-func NewServer(ip string, port int) *Server {
-	server := &Server{Ip: ip, Port: port}
-	return server
 }
