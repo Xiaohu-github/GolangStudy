0d15093375a0ceacbe4b0e7655d427c96ec52ed3
HU <hubotao1998@gmail.com>
2022-07-30 11:49:52 +0800

    Update ：新增客户端菜单栏

--- 16-project/client.go
+++ 16-project/client.go
@@ -11,12 +11,14 @@ type Client struct {
 	ServerPort int
 	name       string
 	connet     net.Conn
+	flag       int //记录当前客户端的模式
 }
 
 func NewCLient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
@@ -28,6 +30,48 @@ func NewCLient(serverIp string, serverPort int) *Client {
 	client.connet = conn
 
 	return client
+}
+
+func (client *Client) menu() bool {
+	var flag int
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	//从标准输入读取的文本
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>>请输入合法范围内的数字<<<<<")
+		return false
+	}
+
+}
+
+func (clinet *Client) Run() {
+	for clinet.flag != 0 {
+		for clinet.menu() != true {
+
+		}
+		//根据不同的模式处理不同的业务
+		switch clinet.flag {
+
+		case 1:
+			fmt.Println("公聊模式")
+			break
+		case 2:
+			fmt.Println("私聊模式")
+			break
+		case 3:
+			fmt.Println("更新用户名")
+			break
+		}
+
+	}
 }
 
 var serverIp string
@@ -53,5 +97,5 @@ func main() {
 	fmt.Println(">>>>>>>>连接成功...")
 
 	//启动客户端的业务
-	select {}
+	client.Run()
 }
