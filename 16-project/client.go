c8abb3b3813389a250d385df7b6672f6e5334cf8
HU <hubotao1998@gmail.com>
2022-07-31 14:44:12 +0800

    20220731-14
    新增更改用户名
    新增服务端返回数据监听

--- 16-project/client.go
+++ 16-project/client.go
@@ -3,13 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
 	ServerIp   string
 	ServerPort int
-	name       string
+	Name       string
 	connet     net.Conn
 	flag       int //记录当前客户端的模式
 }
@@ -32,6 +34,12 @@ func NewCLient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+func (client *Client) DealResponse() {
+	//如果client.connet有数据，就直接copy到stdout标准输出上
+	//永久阻塞=>for{}
+	io.Copy(os.Stdout, client.connet)
+}
+
 func (client *Client) menu() bool {
 	var flag int
 	fmt.Println("1.公聊模式")
@@ -52,6 +60,20 @@ func (client *Client) menu() bool {
 
 }
 
+func (clinet *Client) updateName() bool {
+	fmt.Println(">>>>>>请输入用户名")
+	fmt.Scanln(&clinet.Name)
+
+	sendMsg := "rename|" + clinet.Name + "\n"
+	_, err := clinet.connet.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return false
+	}
+
+	return true
+}
+
 func (clinet *Client) Run() {
 	for clinet.flag != 0 {
 		for clinet.menu() != true {
@@ -62,12 +84,29 @@ func (clinet *Client) Run() {
 
 		case 1:
 			fmt.Println("公聊模式")
+			var chatMsg string
+			fmt.Println(">>>>>>请输入聊天内容,exit退出")
+			fmt.Scanln(&chatMsg)
+			for chatMsg != "exit" {
+				if len(chatMsg) != 0 {
+					sendMsg := chatMsg + "\n"
+					_, err := clinet.connet.Write([]byte(sendMsg))
+					if err != nil {
+						fmt.Println("connet Write error:", err)
+						break
+					}
+				}
+				chatMsg = ""
+				fmt.Println(">>>>>>请输入聊天内容,exit退出")
+				fmt.Scanln(&chatMsg)
+			}
 			break
 		case 2:
 			fmt.Println("私聊模式")
 			break
 		case 3:
 			fmt.Println("更新用户名")
+			clinet.updateName()
 			break
 		}
 
@@ -96,6 +135,7 @@ func main() {
 	}
 	fmt.Println(">>>>>>>>连接成功...")
 
+	go client.DealResponse()
 	//启动客户端的业务
 	client.Run()
 }
